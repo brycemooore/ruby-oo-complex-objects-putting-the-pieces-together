a79790df03700cae53f8991f9372fba977e22d6a
Bryce Moore <brycemooore@gmail.com>
2020-10-07 17:22:10 -0400

    finished lab

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :color
+    attr_accessor :brand
+    attr_accessor :material
+    attr_accessor :condition
+    attr_accessor :size
+
+    def initialize(brand)
+        @brand = brand
+    end 
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        self.condition = "new"
+    end 
+end
